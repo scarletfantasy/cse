d584a42b1a4274744e3068c78061ca8a2d5768ae
shenruien <shenruien@sjtu.edu.cn>
2019-12-12 09:40:27 +0800

    c1

--- handson3/deploy.sh
+++ handson3/deploy.sh
@@ -15,7 +15,13 @@ sudo docker run -d --net ss --ip 172.20.0.8 --add-host  carts-db:172.20.0.3  --a
 sleep 2
 sudo docker run -d --net ss --ip 172.20.0.9 --add-host  orders-db:172.20.0.3   --add-host  user:172.20.0.7 --add-host  carts:172.20.0.8 --add-host  payment:172.20.0.6 --add-host  shipping:172.20.0.5  weaveworksdemos/orders:0.4.7
 sleep 2
-sudo docker run -d --net ss --ip 172.20.0.10 --add-host  catalogue:172.20.0.4 --add-host  orders:172.20.0.9   --add-host  user:172.20.0.7 --add-host  carts:172.20.0.8 --add-host  payment:172.20.0.6 --add-host  shipping:172.20.0.5  -p 1234:8079 dplsming/sockshop-frontend:0.1
+sudo docker run -d --net ss --ip 172.20.0.10 --add-host session-db:172.20.0.13 --add-host  catalogue:172.20.0.4 --add-host  orders:172.20.0.9   --add-host  user:172.20.0.7 --add-host  carts:172.20.0.8 --add-host  payment:172.20.0.6 --add-host  shipping:172.20.0.5  -e SESSION_REDIS=1 dplsming/sockshop-frontend:0.1
 sleep 2
-sudo docker run -d --net ss --ip 172.20.0.11 -v /home/shenruien/Desktop/handson3/handson3/conf.d:/etc/nginx/conf.d  -v  /home/shenruien/Desktop/handson3/handson3/frontend/public:/usr/public -p 12345:12345 nginx:latest
+sudo docker run -d --net ss --ip 172.20.0.11 --add-host session-db:172.20.0.13 --add-host  catalogue:172.20.0.4 --add-host  orders:172.20.0.9   --add-host  user:172.20.0.7 --add-host  carts:172.20.0.8 --add-host  payment:172.20.0.6 --add-host  shipping:172.20.0.5  -e SESSION_REDIS=1 dplsming/sockshop-frontend:0.1
+sleep 2
+sudo docker run -d --net ss --ip 172.20.0.12 --add-host session-db:172.20.0.13 --add-host  catalogue:172.20.0.4 --add-host  orders:172.20.0.9   --add-host  user:172.20.0.7 --add-host  carts:172.20.0.8 --add-host  payment:172.20.0.6 --add-host  shipping:172.20.0.5  -e SESSION_REDIS=1 dplsming/sockshop-frontend:0.1
+sleep 2
+sudo docker run -d --net ss --ip 172.20.0.13 redis:latest
+sleep 2
+sudo docker run -d --net ss --ip 172.20.0.14 -v $(pwd)/conf.d:/etc/nginx/conf.d  -v  $(pwd)/frontend/public:/usr/public -p 12345:12345 nginx:latest
 
---
+++ handson3/test.sh
@@ -0,0 +1,1 @@
+docker run -it --rm dplsming/sockshop-test:0.1 http://172.17.0.1:12345 correctness
